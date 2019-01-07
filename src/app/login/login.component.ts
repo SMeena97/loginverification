3b9d9a4f6f56b59b8e49b9255b21d3a4f21f6dfa
SMeena97 <smeena.ece13@gmail.com>
2019-01-07 17:42:54 +0530

    first commit

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,51 @@
+import { Component } from '@angular/core';
+import { Router } from "@angular/router";
+import { HttpClient} from '@angular/common/http';
+import 'rxjs/add/operator/map';
+import "rxjs/Rx";
+
+interface DataResponse{
+    firstname: string;
+    lastname: string;
+    email: string;
+    password: string;
+    id: number;
+}
+@Component({
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+ 
+    public input: any;
+    constructor(private http: HttpClient) {
+        this.input = {
+            "email": "",
+            "password": ""
+        };
+    }
+
+    
+ public verification(){
+     var tempArray:DataResponse[]=[];
+     this.http.get('http://localhost:3000/account').subscribe((data:DataResponse[])=>{
+         tempArray=data
+
+         var j:number;
+   j=0;
+       for(var i=0;i<tempArray.length;i++){
+       if(this.input.email===tempArray[i].email && this.input.password===tempArray[i].password){
+       alert("successfully loggedIn");
+      break;
+       }
+       j++;
+    }
+         if(j==i)
+         alert("Invalid data");
+         
+        
+        }); 
+   
+     }
+    }
